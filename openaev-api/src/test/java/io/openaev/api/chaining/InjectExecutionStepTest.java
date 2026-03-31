3798019df0c3ca60de398a59045363534b79b740
Gael Leblan <gael.leblan@filigran.io>
2026-03-31 18:45:03 +0200

    [backend/frontend] feat(catalog): injectors share injector contracts (#4862)

--- openaev-api/src/test/java/io/openaev/api/chaining/InjectExecutionStepTest.java
+++ openaev-api/src/test/java/io/openaev/api/chaining/InjectExecutionStepTest.java
@@ -440,7 +440,7 @@ public class InjectExecutionStepTest {
 
     // ASSERT
     Assertions.assertEquals(
-        "Injector not found for injectorId  and step (READY) ID null", ex.getMessage());
+        "Injector not found for inject null and step (READY) ID null", ex.getMessage());
   }
 
   @Test
